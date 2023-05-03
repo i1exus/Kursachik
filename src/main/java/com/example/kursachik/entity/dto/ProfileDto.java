4929c691347d73fd0988e3ad5a2ca49fbc1d68f1
i1exus <99401452+i1exus@users.noreply.github.com>
2023-05-04 01:16:10 +0300

    First chance

---
+++ src/main/java/com/example/kursachik/entity/dto/ProfileDto.java
@@ -0,0 +1,23 @@
+package com.example.kursachik.entity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProfileDto {
+    private Long id;
+
+
+    private String name;
+    private String surname;
+    private String patronymic;
+    private String email;
+    private String phone;
+}
